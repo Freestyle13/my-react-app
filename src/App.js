6668b3e940092de8fa015b3ce6c24133c7876171
Freestyle13 <kpklotz@comcast.net>
2023-08-28 21:09:40 -0500

    props children

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./Pages/AllMeetups";
 import NewMeetupsPage from "./Pages/NewMeetup";
 import Favorites from "./Pages/Favorites";
-import MainNavigation from "./Components/Layout/MainNavigation";
+import Layout from "./Components/Layout/Layout";
 
 function App() {
   return <div>
-    <MainNavigation />
+    <Layout>
     <Routes>
     <Route path="/" element={<AllMeetupsPage />}/>
     <Route path="/new-meet-up" element={<NewMeetupsPage />}/>
     <Route path="/favorites" element={<Favorites />}/>
     </Routes>
+    </Layout>
   </div>
   ;
 }
