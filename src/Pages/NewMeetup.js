862ebf3db1b8d4347ff384275c48c9dc801e88fd
Freestyle13 <kpklotz@comcast.net>
2023-08-31 19:20:24 -0500

    Configure Firebase and add POST

--- src/Pages/NewMeetup.js
+++ src/Pages/NewMeetup.js
@@ -1,9 +1,23 @@
 import NewMeetupForm from "../Components/Meetups/NewMeetupForm";
 
 function NewMeetupsPage(){
+    const apiKey = "AIzaSyAj3yybiNCSe02YGRwx28hBglBJp3LsaGw";
+
+    function addMeetupHandler(meetupData){
+        fetch('https://react-getting-started-9bd89-default-rtdb.firebaseio.com/meetups.json?auth=${apiKey}',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        );
+    }
+
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm/>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>;
 }
 
