21b14ea5181ba9b255c106d78ce309de0f435351
Freestyle13 <kpklotz@comcast.net>
2023-08-31 19:46:59 -0500

    navigate programatically

--- src/Pages/NewMeetup.js
+++ src/Pages/NewMeetup.js
@@ -1,7 +1,9 @@
 import NewMeetupForm from "../Components/Meetups/NewMeetupForm";
+import { useNavigate } from 'react-router-dom';
 
 function NewMeetupsPage(){
     const apiKey = "AIzaSyAj3yybiNCSe02YGRwx28hBglBJp3LsaGw";
+    const history = useNavigate();
 
     function addMeetupHandler(meetupData){
         fetch('https://react-getting-started-9bd89-default-rtdb.firebaseio.com/meetups.json?auth=${apiKey}',
@@ -12,7 +14,9 @@ function NewMeetupsPage(){
                 'Content-Type': 'application/json'
             }
         }
-        );
+        ).then(() => {
+            history('/');
+        });
     }
 
     return <section>
